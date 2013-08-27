aa810a817fbf7286ed428777e5a3965a43322960
schereja <schereja@10.30.17.193>
2013-08-26 21:10:21 -0500

    Updated Concrete Package with properties

--- src/concretepackage/Employee.java
+++ src/concretepackage/Employee.java
@@ -9,5 +9,19 @@ package concretepackage;
  * @author schereja
  */
 public class Employee {
-    
+    public int employeeId;
+    public String firstName;
+    public String lastName;
+    public boolean employed;
+}
+class HourlyEmployee extends Employee{
+    private double payPerHour;
+}
+
+class SalariedEmployee extends Employee{
+    private double salary;
+}
+
+class SalaryPlusBonusEmployee extends SalariedEmployee{
+    private double salaryWithBonus;
 }
